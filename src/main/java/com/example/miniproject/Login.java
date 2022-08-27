b88b08880c3f7aee941cca8f86aa0ac9e358c4fd
Varad J <varad.gcs@gmail.com>
2022-08-27 12:04:38 -0700

    more cleanup

--- src/main/java/com/example/miniproject/Login.java
+++ src/main/java/com/example/miniproject/Login.java
@@ -1,7 +1,6 @@
 package com.example.miniproject;
 
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
