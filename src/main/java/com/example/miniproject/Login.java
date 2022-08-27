dfdd32cf858a1ceb02b63b958fe12cdf1e480224
Varad J <varad.gcs@gmail.com>
2022-08-27 02:16:15 -0700

    test

---
+++ src/main/java/com/example/miniproject/Login.java
@@ -0,0 +1,23 @@
+package com.example.miniproject;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Login extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Login.class.getResource("Login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 640, 320);
+        stage.setTitle("Login");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.miniproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.miniproject to javafx.fxml;
+    exports com.example.miniproject;
+}
