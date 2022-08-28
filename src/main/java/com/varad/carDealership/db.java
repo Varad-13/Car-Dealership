8010386db3dad8647432cc8b32ac5a0c3d70a25b
Varad J <varad.gcs@gmail.com>
2022-08-28 12:43:34 +0530

    Added functionality to registration form (data now gets saved)

--- src/main/java/com/varad/carDealership/db.java
+++ src/main/java/com/varad/carDealership/db.java
@@ -9,17 +9,30 @@ public class db {
     private static final String DATABASE_URL = "jdbc:mysql://database-1.cj5xtfkwqhiq.ap-south-1.rds.amazonaws.com/mini_project";
     private static final String DATABASE_USERNAME = "admin";
     private static final String DATABASE_PASSWORD = "genius123";
-    private static final String INSERT_QUERY = "INSERT INTO loginDetails (uname, uemail, upasswd) VALUES (?, ?, ?)";
+    private static final String INSERT_LOGIN = "INSERT INTO loginDetails (uname, uemail, upasswd) VALUES (?, ?, ?)";
+    private static final String INSERT_DATA = "INSERT INTO userData (uname, uemail, uadd) VALUES (?, ?, ?)";
     public void insertLoginDetails(String name, String email, String passwd) throws SQLException {
         try (Connection connection = DriverManager
                 .getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
-             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_QUERY)) {
+             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_LOGIN)) {
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, email);
             preparedStatement.setString(3, passwd);
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
-            // print SQL exception information
+            printSQLException(e);
+        }
+    }
+
+    public void insertUserData(String name, String email, String address) throws SQLException{
+        try (Connection connection = DriverManager
+                .getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
+            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_DATA)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, email);
+            preparedStatement.setString(3, address);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
             printSQLException(e);
         }
     }
--- src/main/java/com/varad/carDealership/registerController.java
+++ src/main/java/com/varad/carDealership/registerController.java
@@ -2,9 +2,9 @@ package com.varad.carDealership;
 import javafx.fxml.*;
 import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.sql.SQLException;
@@ -19,6 +19,8 @@ public class registerController {
     @FXML
     private TextField temail;
     @FXML
+    private TextArea taddress;
+    @FXML
     protected void onConfirmButtonClick() {
         try {
             Stage stage = (Stage) Confirm.getScene().getWindow();
@@ -26,8 +28,10 @@ public class registerController {
             String fullName = tname.getText();
             String emailId = temail.getText();
             String password = passwd.getText();
+            String address = taddress.getText();
             com.varad.carDealership.db jdbc = new db();
             jdbc.insertLoginDetails(fullName, emailId, password);
+            jdbc.insertUserData(fullName, emailId, address);
             FXMLLoader fxmlLoader = new FXMLLoader(driver.class.getResource("login.fxml"));
             Scene scene = new Scene(fxmlLoader.load(), 640, 320);
             Stage window = new Stage();
